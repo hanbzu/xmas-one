2577cd15cb4fa2dd4bbec5e40914374976d14312
Hibai Unzueta <hibai.unzueta@gmail.com>
2020-09-30 21:00:57 +0100

    Added next-pwa based PWA conf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 
+const APP_NAME = "next-pwa example";
+const APP_DESCRIPTION = "This is an example of using next-pwa plugin";
+
 function XmasOne({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="description" content="xmas-one description" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icon-160.png"
+          rel="icon"
+          type="image/png"
+          sizes="160x160"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default XmasOne;
